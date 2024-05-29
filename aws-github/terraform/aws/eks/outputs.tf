7ec2b500df9e3c2bd57f6bcd26c9ea22745f1459
Adao Santos <djadao@gmail.com>
2024-05-29 00:49:19 -0300

    fix nodegroup iam

--- aws-github/terraform/aws/eks/outputs.tf
+++ aws-github/terraform/aws/eks/outputs.tf
@@ -1,5 +1,5 @@
 output "node_iam_role_name" {
-  value = module.eks.eks_managed_node_groups.default_node_group.iam_role_name
+  value = module.eks.eks_managed_node_groups.karpenter.iam_role_name
 }
 
 output "external_dns_policy_arn" {
